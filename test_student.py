9998a0ada783f4eb51dd3de18d203df040236475
Dan Morriss <danielmorriss1@gmail.com>
2023-10-10 10:21:03 +0100

    Add setup and teardown classes and methods for testing

--- test_student.py
+++ test_student.py
@@ -2,24 +2,45 @@ import unittest
 from student import Student
 
 class TestStudent(unittest.TestCase):
+    
+    #runs before testing and tests, this is a class method not for the instance of a class
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+    
+    
+    #runs after testing, again a class method
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+    
+    
+    # runs before each test
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+
+    #runs after each test
+    def tearDown(self):
+        print('tearDown')
+
 
     def test_full_name(self):
-        student = Student('John', 'Doe')
-        
-        self.assertEqual(student.full_name, 'John Doe')
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
     
     
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('test_alert_santa')
+        self.student.alert_santa()
         
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
     
     
     def test_email(self):
-        student = Student('John', 'Doe')
-        
-        self.assertEqual(student.email, 'john.doe@email.com')
+        print('test_email')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
 
 
 if __name__ == '__main__':
